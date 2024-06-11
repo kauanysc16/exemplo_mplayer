3201878a781c6be78f4af21ac659df5e893d164b
kauanysc16 <kauanycamilo91@gmail.com>
2024-06-11 16:59:49 -0300

    first commit

---
+++ lib/services/music_controller.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+import '../models/music_model.dart';
+
+class MusicService {
+  List<MusicModel> _list = [];
+  List<MusicModel> get list => _list;
+
+  final String url = "http://192.168.137.1:3000/music";
+
+  //fetchList()
+  Future<List<MusicModel>> fetchList() async{
+    final Response  response = await http.get(Uri.parse(url));
+    if(response.statusCode == 200){
+      final List<dynamic> list = json.decode(response.body);
+      _list.clear();
+      _list.addAll(list.map((e) => MusicModel.fromMap(e)).toList());
+      return _list;
+    }
+    return [];
+
+  }
+}
